62a874473d4f0f05ef6779e45b1f1612dd5bbf75
mrklika <pe.klika@gmail.com>
2025-08-08 23:30:23 +0200

    API adapted to training app

--- src/api/training/routes/training.js
+++ src/api/training/routes/training.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * author router.
+ * training router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::author.author');
+module.exports = createCoreRouter('api::training.training');
