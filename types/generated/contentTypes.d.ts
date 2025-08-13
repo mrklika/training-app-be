6ef84e2c0220f5c49a9d9051638fc230a23c2d52
mrklika <pe.klika@gmail.com>
2025-08-13 16:17:46 +0200

    model changed

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -457,7 +457,7 @@ export interface ApiUserTrainingUserTraining
     certification: Schema.Attribute.Media<
       'images' | 'videos' | 'audios' | 'files'
     >;
-    completed: Schema.Attribute.Boolean;
+    completed: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     completeDate: Schema.Attribute.Date;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
@@ -953,6 +953,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    fullName: Schema.Attribute.String & Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
